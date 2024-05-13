e1501c5aa5e15ec857f813b1bfb1e37261fa1a18
Shoukat Ali <shoukat.1983@gmail.com>
2024-05-13 14:13:09 -0600

    Directory structure, header file, and required library files added

---
+++ header/conn_dis_token.hpp
@@ -0,0 +1,33 @@
+/**
+ * This program is an attempt to show the followings
+ * 
+ *      1. Load the HSM library by setting an environment variable HSM_PKCS_11 in order to use PKCS#11 library
+ *      2. Connect to valid slot using the followings
+ *          i.      C_Initialize() 
+ *          ii.     C_OpenSession() 
+ *          iii.    C_Login()
+ *      3. Disconnect from a connect slot using the followings
+ *          i.      C_Logout() 
+ *          ii.     C_CloseSession() 
+ *          iii.    C_Finalize()
+ * 
+*/
+
+
+#ifndef CONN_DISCONN_HPP
+#define CONN_DISCONN_HPP
+
+#include <string>
+#include <cryptoki.h>   // exist in include directory in the same program directory with gcc use -Iinclude
+
+int check_Operation(CK_RV rv, const char* message);
+
+int load_library_HSM(void*& libHandle, CK_FUNCTION_LIST_PTR& funclistPtr);
+
+int connect_Slot(CK_FUNCTION_LIST_PTR funclistPtr, CK_SESSION_HANDLE& hSession, std::string& usrPIN);
+
+int disconnect_Slot(CK_FUNCTION_LIST_PTR funclistPtr, CK_SESSION_HANDLE& hSession);
+
+void free_Resource(void*& libHandle, CK_FUNCTION_LIST_PTR& funclistPtr, std::string& usrPIN);
+
+#endif
