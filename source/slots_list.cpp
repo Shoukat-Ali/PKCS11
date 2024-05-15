539631834e3475c63bd9b2c554bbaed7e9b0b7a5
Shoukat Ali <shoukat.1983@gmail.com>
2024-05-15 15:40:40 -0600

    First version of the implementation file of slot_list is added

---
+++ source/slots_list.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <limits>
+#include <dlfcn.h>		// Required for dynamic loading, linking e.g., dlopen(), dlclose(), dlsym(), etc.
+#include "../header/conn_dis_token.hpp"
+ 
+
+using std::cout; 
+using std::cin;
+using std::endl;
+
+
+
+/**
+ * The function checks if a requested Cryptoki (PKCS #11) operation was a success or not.
+ * 
+ * rv represents the CK_RV value returned by Cryptoki function
+ * message represent the Cryptoki operation 
+ * 
+ * If the CK_RV value is CKR_OK, then the operation was success and 0 is returned. 
+ * Otherwise, non-zero integer is returned on failure.
+ *  
+*/
+int check_Operation(const CK_RV rv, const char* message)
+{
+	if (rv != CKR_OK) {
+		cout << "Error, " << message << " failed with : " << rv << endl
+			 << "RV : " << rv << endl;
+		return 1;
+	}
+	return 0;
+}
+
+
+
+/**
+ * The function attempts to load SoftHSM library in order to use PKCS# 11 functions/API.
+ * 
+ * libHandle is a void pointer for SoftHSM library handle
+ * funclistPtr is an alias of pointer to the list of functions i.e., CK_FUNCTION_LIST_PTR
+ *  
+ * On success, integer 0 is returned. Otherwise, non-zero integer is returned.
+*/
+int load_library_HSM(void*& libHandle, CK_FUNCTION_LIST_PTR& funclistPtr)
+{
+	const char *libPath = nullptr;
+	
+	libPath = getenv("SOFTHSM2_LIB");
+	if(libPath == nullptr) {
+		cout << "Error, SOFTHSM2_LIB environment variable is not set" << endl;
+		return 2;
+	}
+	
+	libHandle = dlopen(libPath, RTLD_NOW);
+	if (!libHandle) {
+		cout << "Error, failed to load SoftHSM library into memory from path " << libPath << endl;
+		return 3;
+	}
+	
+    CK_C_GetFunctionList C_GetFunctionList = (CK_C_GetFunctionList) dlsym(libHandle, "C_GetFunctionList");
+	if (!C_GetFunctionList) {
+		cout << "Error, dlsym() failed to find loaded SoftHSM library" << endl;
+		return 3;
+	}
+	
+    return check_Operation(C_GetFunctionList(&funclistPtr), "C_GetFunctionList");
+	
+}
+
+
+
+
+/**
+ * The functions attempts to perform cleanup by freeing memory/resources
+ * First, decrements the reference count on SoftHSM library handle
+ * Second, assigning null to the pointer to the list of PKCS #11 function
+ * Lastily, removing/clearing the user PIN
+ * 
+ * The function does not return anything 
+*/
+void free_Resource(void*& libHandle, CK_FUNCTION_LIST_PTR& funclistPtr, std::string& usrPIN)
+{
+	cout << "Clean up and free the resources\n";
+	
+    if (dlclose(libHandle)) {
+		cout << "Error, dlclose() on softHSM library reference count\n";
+	}
+    funclistPtr = NULL_PTR;
+	
+    // Removes all characters from the usrPIN string and all pointers, references, and iterators are invalidated. 
+    usrPIN.clear();
+}
