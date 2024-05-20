165ffac1d195658963a8b23125ca8fbdd84ff7e2
Shoukat Ali <shoukat.1983@gmail.com>
2024-05-20 14:17:14 -0600

    Header file of gen_ECDSA_keypair added

---
+++ header/gen_ECDSA_keypair.hpp
@@ -0,0 +1,21 @@
+/**
+ * This program is an attempt to show the following operations
+ * 
+ *  
+*/
+
+
+#ifndef ECDSA_KEYPAIR_HPP
+#define ECDSA_KEYPAIR_HPP
+
+#include <cryptoki.h>   // exist in include directory in the same program directory with gcc use -I/path/to/include
+
+int check_operation(const CK_RV rv, const char* message);
+
+int load_library_HSM(void*& libHandle, CK_FUNCTION_LIST_PTR& funclistPtr);
+
+void free_resource(void*& libHandle, CK_FUNCTION_LIST_PTR& funclistPtr);
+
+int display_all_slot_token(const CK_FUNCTION_LIST_PTR funclistPtr);
+
+#endif
