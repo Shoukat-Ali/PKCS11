52faa7a3d863e55f4709f209690c7e75f1465374
Shoukat Ali <shoukat.1983@gmail.com>
2024-05-30 12:08:40 -0600

    Description added for AES 128-bit key generation function

--- source/gen_AES_keys.cpp
+++ source/gen_AES_keys.cpp
@@ -8,12 +8,11 @@ using std::cin;
 using std::endl;
 
 /**
- * The CKM_ECDSA denotes ECDSA without hashing mechanism.
- * It is a mechanism for single-part signatures and verification for ECDSA
  * This mechanism does not have a parameter
  * 
  * */
-CK_MECHANISM signMech = {CKM_ECDSA};
+CK_MECHANISM keyMech = {CKM_AES_KEY_GEN};
+
 
 
 /**
@@ -145,14 +144,15 @@ int connect_slot(const CK_FUNCTION_LIST_PTR funclistPtr, CK_SESSION_HANDLE& hSes
 }
 
 /**
- * The function generates AES 128-bit key
+ * The function generates AES 128-bit secret key
  * 
  * funclistPtr is a pointer to the list of functions i.e., CK_FUNCTION_LIST_PTR
  * hSession is an alias of session ID/handle
  * 
  * On success, integer 0 is returned. Otherwise, non-zero integer is returned.
 */
-int gen_AES_128_key(const CK_FUNCTION_LIST_PTR funclistPtr, CK_SESSION_HANDLE& hSession)
+int gen_AES_128_key(const CK_FUNCTION_LIST_PTR funclistPtr, CK_SESSION_HANDLE& hSession,
+					CK_OBJECT_HANDLE_PTR keyhandPtr)
 {
 	int retVal = 0;
 
@@ -161,7 +161,6 @@ int gen_AES_128_key(const CK_FUNCTION_LIST_PTR funclistPtr, CK_SESSION_HANDLE& h
 		return 4;
 	}
 
-    CK_MECHANISM keyMech = {CKM_AES_KEY_GEN};
     CK_BBOOL yes = CK_TRUE;
     CK_BBOOL no = CK_FALSE;
     CK_UTF8CHAR keyLabel[] = "AES 128-bit key";
@@ -172,7 +171,7 @@ int gen_AES_128_key(const CK_FUNCTION_LIST_PTR funclistPtr, CK_SESSION_HANDLE& h
         {CKA_PRIVATE,			&yes,			sizeof(yes)},
         {CKA_SENSITIVE,			&yes,			sizeof(yes)},
         {CKA_EXTRACTABLE,		&yes,			sizeof(yes)},
-        {CKA_MODIFIABLE,		&yes,			sizeof(yes)},
+        {CKA_MODIFIABLE,		&no,			sizeof(no)},
         {CKA_ENCRYPT,			&yes,			sizeof(yes)},
         {CKA_DECRYPT,			&yes,			sizeof(yes)},
         {CKA_LABEL,				&keyLabel,		sizeof(keyLabel)},
@@ -180,11 +179,27 @@ int gen_AES_128_key(const CK_FUNCTION_LIST_PTR funclistPtr, CK_SESSION_HANDLE& h
     };
 
     /**
+	 * CK_RV C_GenerateKey(CK_SESSION_HANDLE hSession, 
+	 * 						CK_MECHANISM_PTR pMechanism,
+	 * 						CK_ATTRIBUTE_PTR pTemplate,
+	 * 						CK_ULONG ulCount,
+	 * 						CK_OBJECT_HANDLE_PTR phKey);
 	 * 
+	 * C_GenerateKey() generates a secret key or set of domain parameters, creating a new object. 
+	 * 
+	 * hSession is the session’s handle; 
+	 * pMechanism points to the generation mechanism; 
+	 * pTemplate points to the template for the new key or set of domain parameters; 
+	 * ulCount is the number of attributes in the template; 
+	 * phKey points to the location that receives the handle of the new key or set of domain parameters.
+	 * 
+	 * Since the type of key or domain parameters to be generated is implicit in the generation
+	 * mechanism, the template does not need to supply a key type. The CKA_CLASS attribute is
+	 * treated similarly.
 	*/
     retVal = check_operation(funclistPtr->C_GenerateKey(hSession, &keyMech, keyAttrb, 
 														sizeof(keyAttrb) / sizeof(*keyAttrb), 
-														&objHandle), "C_GenerateKey()");
+														keyhandPtr), "C_GenerateKey()");
 
     return retVal;
 }
