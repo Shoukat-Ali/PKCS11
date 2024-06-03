70f4acaa52db5b104d4119b785ecd8b9afd75235
Shoukat Ali <shoukat.1983@gmail.com>
2024-06-03 14:41:18 -0600

    AES encryption function with C_EncryptInit()

--- source/AES_enc_dec.cpp
+++ source/AES_enc_dec.cpp
@@ -7,48 +7,84 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-/**
- * This mechanism does not have a parameter
- * 
- * */
-CK_MECHANISM keyMech = {CKM_AES_KEY_GEN};
-
-
 
 /**
- * The function checks if a requested Cryptoki (PKCS #11) operation was a success or not.
+ * An initialization vector (IV) is used by several modes to randomize the encryption 
+ * such that if the same plaintext is encrypted multiple times, then distinct ciphertexts
+ * are produced. Usually, an IV usually does not need to be secret. 
+ * For most block cipher modes, it is important that an IV is never reused under the same key.
  * 
- * rv represents the CK_RV value returned by Cryptoki function
- * message represent the Cryptoki operation 
- * 
- * If the CK_RV value is CKR_OK, then the operation was success and 0 is returned. 
- * Otherwise, non-zero integer is returned on failure.
- *  
+ * TODO: generate random IV  
 */
-int check_operation(const CK_RV rv, const char* message)
+CK_BYTE IV[] = {"UTf34-ijhy;it1MM"};
+
+
+/**
+ * 
+ * CK_MECHANISM is a structure that specifies a particular mechanism and any parameters it requires.
+ * 
+ * typedef struct CK_MECHANISM {
+ *              CK_MECHANISM_TYPE mechanism;
+ *              CK_VOID_PTR pParameter;
+ *              CK_ULONG ulParameterLen;
+ * } CK_MECHANISM;
+ * 
+ * mechanismthe type of mechanism;
+ * pParameterpointer to the parameter if required by the mechanism;
+ * ulParameterLen length in bytes of the parameter
+ * 
+ * AES-CBC with PKCS padding, denoted CKM_AES_CBC_PAD, is a mechanism for
+ * single- and multiple-part encryption and decryption.
+ * It has a parameter, a 16-byte initialization vector.
+
+*/
+CK_MECHANISM encMech = {CKM_AES_CBC_PAD, IV, sizeof(IV)-1};
+
+
+
+/**
+ * The function encrypts given data using Advanced Encryption Standard (AES) with 
+ * Cipher block chaining (CBC) mode i.e., CKM_AES_CBC_PAD
+ * 
+ * funclistPtr is a pointer to the list of functions i.e., CK_FUNCTION_LIST_PTR
+ * ptPtr is a pointer to array of unsinged 8-bit character representing plaintext
+ * ctPtr is a pointer to array of unsinged 8-bit character representing ciphertext
+ * 
+ * On success, integer 0 is returned. Otherwise, non-zero integer is returned.
+*/
+int encrypt_data(const CK_FUNCTION_LIST_PTR funclistPtr, CK_SESSION_HANDLE& hSession,
+                const CK_OBJECT_HANDLE& hSecretkey,
+                CK_CHAR_PTR ptPtr, CK_BYTE_PTR ctPtr)
 {
-	if (rv != CKR_OK) {
-		cout << "Error, " << message << " failed with RV : " << rv << endl;
-		return 1;
+    int retVal = 0;
+
+    // Checking whether funclistPtr is null or not 
+	if (is_nullptr(funclistPtr) || is_nullptr(ptPtr)) {
+		return 4;
 	}
-	return 0;
+
+    /**
+     * CK_RV C_EncryptInit(CK_SESSION_HANDLE hSession,
+     *                      CK_MECHANISM_PTR pMechanism,
+     *                      CK_OBJECT_HANDLE hKey);
+     * 
+     * C_EncryptInit() initializes an encryption operation. 
+     * 
+     * hSession is the session’s handle;
+     * pMechanism points to the encryption mechanism; 
+     * hKey is the handle of the encryption key.
+     * 
+     * After calling C_EncryptInit(), the application can either call C_Encrypt() to encrypt data
+     * in a single part; or call C_EncryptUpdate() zero or more times, followed by
+     * C_EncryptFinal(), to encrypt data in multiple parts. The encryption operation is active
+     * until the application uses a call to C_Encrypt() or C_EncryptFinal() to actually obtain the
+     * final piece of ciphertext.
+    */
+	retVal = check_operation(funclistPtr->C_EncryptInit(hSession, &encMech, hSecretkey), "C_EncryptInit()");
+    if (!retVal) {
+        // The encryption operation successfully initialized
+
+    }
+	return retVal;
 }
 
-
-
-/**
- * This function checks whether a given pointer is null of not.
- * 
- * ptr is a constant pointer to void type
- * 
- * If given pointer is null, then return true. Otherwise, faluse is returned.
- * 
-*/
-inline bool is_nullptr(void * const ptr)
-{
-	if (ptr) {
-		return false;
-	}
-	cout << "Error, pointer is NULL\n";
-	return true;
-}
